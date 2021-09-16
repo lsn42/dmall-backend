b45ac9e1100ae5a1f6a517cbd1a6bba72c17ca8e
lsn42 <24622966+lsn42@users.noreply.github.com>
2021-09-16 12:18:13 +0800

    init

---
+++ src/main/java/com/example/dmall/DmallApplication.java
@@ -0,0 +1,13 @@
+package com.example.dmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dmall/DmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
